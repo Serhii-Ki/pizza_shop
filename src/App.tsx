6fa76ef68f7118d107cbb2a841b7fe639bb97bfc
Kiba Sergey <serhiikiba@gmail.com>
2024-04-08 22:53:44 +0200

    init commit

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import './App.css'
+import CustomBtn from "./components/customBtn/CustomBtn.tsx";
+
+function App() {
+  return (
+    <>
+      <CustomBtn title={'some'} onClick={() => {}}/>
+    </>
+  )
+}
+
+export default App
---
+++ src/components/customBtn/CustomBtn.tsx
@@ -0,0 +1,18 @@
+import styles from './customBtn.module.css';
+
+type ButtonPropsType = {
+  title: string;
+  spanTitle?: string;
+  onClick: () => void;
+}
+
+function CustomBtn(props: ButtonPropsType) {
+  return (
+      <button className={styles['btn']} onClick={props.onClick}>
+        <span>{props.spanTitle}</span>
+        {props.title}
+      </button>
+  );
+}
+
+export default CustomBtn;
