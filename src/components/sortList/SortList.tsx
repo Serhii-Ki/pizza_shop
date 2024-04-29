82339477cbcc1d89d44bd0a594e5bdad90be9f6a
Kiba Sergey <serhiikiba@gmail.com>
2024-04-29 19:53:29 +0200

    added close popup

--- src/components/sortList/SortList.tsx
+++ src/components/sortList/SortList.tsx
@@ -1,6 +1,6 @@
 import styles from './sortList.module.css';
 import cn from 'classnames';
-import {useState} from "react";
+import {useEffect, useRef, useState} from "react";
 import {useDispatch, useSelector} from "react-redux";
 import {AppStore} from "../../redux/store.ts";
 import {setSortProperty, SortNameType, sortPropertyType} from "../../redux/slices/filterSlice.ts";
@@ -29,17 +29,32 @@ const SortList = () => {
   const [isOpen, setIsOpen] = useState(false);
   const sortItem = useSelector<AppStore>(state => state.filter.sort.name) as SortNameType;
   const dispatch = useDispatch();
+  const sortRef = useRef(null);
 
   const toggleSortList = () => {
     setIsOpen(!isOpen);
-  }
+  };
 
   const changeSortItem = (item: SortItemsType) => {
     dispatch(setSortProperty(item));
-  }
+  };
+
+  useEffect(() => {
+    const handleClickOutside = (event: MouseEvent) => {
+      if (sortRef.current && !sortRef.current.contains(event.target as Node)) {
+        setIsOpen(false);
+      }
+    };
+
+    document.body.addEventListener('click', handleClickOutside);
+
+    return () => {
+      document.body.removeEventListener('click', handleClickOutside);
+    };
+  }, [isOpen]);
 
   return (
-      <div className={styles['sort-wrapper']} onClick={toggleSortList}>
+      <div ref={sortRef} className={styles['sort-wrapper']} onClick={toggleSortList}>
         <span>{isOpen ? '\u25B2' : '\u25BC'}</span>
         <span>Сортировать по: </span>
         <span className={styles['sort-item']}>{sortItem}</span>
